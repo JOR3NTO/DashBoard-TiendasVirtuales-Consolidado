53a0bb5ead516f6a58459c9f39b10d5d8f7bc330
JOR3NTO <jorgeedcobo@gmail.com>
2023-11-06 20:41:44 -0500

    OE

--- src/Datos/GraficasDash.java
+++ src/Datos/GraficasDash.java
@@ -24,6 +24,8 @@ public class GraficasDash extends JPanel {
     }
     //hola bro
 
+    //Ole
+
     // --------------------------------------------------------------------------------
     //Consulta fija
     public void GraficoTorta1() {
